1c2b4884357a51680fe9f06b8eac16ca0970417c
DoooongDong <rhkswls98@kakao.com>
2023-03-09 01:50:17 +0900

    first commit

---
+++ src/main/java/dev/be/gptboard/GptBoardApplication.java
@@ -0,0 +1,13 @@
+package dev.be.gptboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GptBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/be/gptboard/GptBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.gptboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
