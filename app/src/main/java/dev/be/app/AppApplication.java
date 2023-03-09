1f8149de321ace47270f86ffd4dea86cfe86ff67
DoooongDong <rhkswls98@kakao.com>
2023-03-09 16:15:56 +0900

    Docker Compose DB 설정

--- app/src/main/java/dev/be/app/AppApplication.java
+++ app/src/main/java/dev/be/app/AppApplication.java
@@ -3,7 +3,14 @@ package dev.be.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = {
+        "dev.be.app",
+        "dev.be.web",
+        "dev.be.common",
+        "dev.be.domain"
+    }
+)
 public class AppApplication {
 
     public static void main(String[] args) {
