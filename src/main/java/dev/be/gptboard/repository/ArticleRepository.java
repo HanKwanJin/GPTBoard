e6921200f0e62c005c84170a438f9173af79f6fa
DoooongDong <rhkswls98@kakao.com>
2023-03-11 23:30:03 +0900

    feature: [#9] querydsl에서 제공되는 검색 특화된 기능을 사용

--- src/main/java/dev/be/gptboard/repository/ArticleRepository.java
+++ src/main/java/dev/be/gptboard/repository/ArticleRepository.java
@@ -1,8 +1,34 @@
 package dev.be.gptboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import dev.be.gptboard.domain.Article;
+import dev.be.gptboard.domain.QArticle;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+    JpaRepository<Article, Long>,
+    QuerydslPredicateExecutor<Article>,
+    QuerydslBinderCustomizer<QArticle>
+{
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+    Page<Article> findByContentContaining(String content, Pageable pageable);
+    Page<Article> findByMember_NicknameContaining(String nickname, Pageable pageable);
 
+    void deleteByIdAndMember_Id(Long articleId, Long memberId);
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
